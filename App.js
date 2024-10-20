90d6c76ee37de7c554a1079df4b7d03f95443dba
WaldnerThomas <twa2384@students.fhv.at>
2024-10-20 18:43:19 +0300

    Added drawer navigation for search, favourites and shopping list

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
+import './gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import Search from './Search';
+import Favourites from './Favourites';
+import ShoppingList from './ShoppingList';
+
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Cocktail Search">
+        <Drawer.Screen name="Cocktail Search" component={Search} />
+        <Drawer.Screen name="Favourites" component={Favourites} />
+        <Drawer.Screen name="Shopping List" component={ShoppingList} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
