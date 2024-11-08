fc75714c9197b54e2f1b0686b6c64fcf823c3caa
WaldnerThomas <twa2384@students.fhv.at>
2024-11-08 17:58:50 +0200

    Added data from database to shoppinglist page

--- ShoppingList.js
+++ ShoppingList.js
@@ -1,7 +1,66 @@
-import { Text } from 'react-native-paper';
+import { Button, IconButton, Text } from 'react-native-paper';
+import { useState, useEffect } from 'react';
+import { getDatabase, push, ref, onValue, remove } from 'firebase/database';
+import { db, signInAnonymouslyFunc, auth } from './FirebaseConfig';
+import { View, FlatList, StyleSheet } from 'react-native';
+
 
 export default function ShoppingList({ navigation }) {
+  const [items, setItems] = useState([]);
+
+  const fetchData = async() => {
+    const userCredential = await signInAnonymouslyFunc(auth);   // gets the unique user id
+    const itemsRef = ref(db, `${userCredential.user.uid}/shoppinglist/`);
+    onValue(itemsRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        setItems(Object.entries(data).map(([key, value]) => ({key,value})));
+        
+      } else {
+        setItems([]);
+      }
+    })  
+  }
+
+  const deleteItem = async(item) => {
+    const userCredential = await signInAnonymouslyFunc(auth);   // gets the unique user id
+    await remove(ref(db, `${userCredential.user.uid}/shoppinglist/${item}`));
+  }
+
+  useEffect(() => {   // makes sure the data gets fetched on entering the page
+    fetchData()
+  }, []);
+
   return (
-    <Text>Shopping List</Text>
+    <View style={styles.container}>
+     <FlatList 
+      renderItem={({item}) => 
+        <View style={styles.itemList}>
+          <Text style={styles.itemName}>• {item.value}</Text>
+          <IconButton
+            icon="check"
+            iconColor="green"
+            mode="default"
+            size={25}
+            onPress={() => deleteItem(item.key)}
+          />
+        </View>} 
+      data={items} />  
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: "5%",
+  },
+  itemList: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  itemName: {
+    fontSize: 18,
+  }
+});
